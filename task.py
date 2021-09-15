cbcda1ace6efc80ad2554ea2b5c9c5b5288444ae
Daniel Raymond <dan@raymond.ch>
2021-09-15 14:06:08 -0400

    Added bloch vector plot

--- task.py
+++ task.py
@@ -1,11 +1,8 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, Aer, execute
+from qiskit.visualization import plot_bloch_multivector
 from typing import List
 import math
-
-# importing Qiskit
-from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, Aer, execute
-
-# import basic plot tools
-from qiskit.visualization import plot_bloch_multivector
+import matplotlib.pyplot as plt
 
 """
 Task 1
@@ -34,22 +31,33 @@ qc.h(qreg_q[0])
 qc.barrier(qreg_q)
 
 # ! Not happy with c_if using bits
-qc.tdg(qreg_q[0]).c_if(qc, 1)
-qc.t(qreg_q[0]).c_if(creg_c[1], 1)
+qc.tdg(qreg_q[0]).c_if(creg_c[0], 1)
+qc.t  (qreg_q[0]).c_if(creg_c[1], 1)
 qc.tdg(qreg_q[0]).c_if(creg_c[2], 1)
-qc.t(qreg_q[0]).c_if(creg_c[3], 1)
-qc.h(qreg_q[0])
+qc.t  (qreg_q[0]).c_if(creg_c[3], 1)
 qc.tdg(qreg_q[1]).c_if(creg_c[0], 1)
 qc.tdg(qreg_q[1]).c_if(creg_c[1], 1)
-qc.t(qreg_q[1]).c_if(creg_c[2], 1)
-qc.t(qreg_q[1]).c_if(creg_c[3], 1)
+qc.t  (qreg_q[1]).c_if(creg_c[2], 1)
+qc.t  (qreg_q[1]).c_if(creg_c[3], 1)
+qc.barrier(qreg_q)
+
+# If only one index is active, move the state to 100% probablity
+qc.tdg(qreg_q[0]).c_if(creg_c, 0b0001)
+qc.t  (qreg_q[0]).c_if(creg_c, 0b0010)
+qc.tdg(qreg_q[0]).c_if(creg_c, 0b0100)
+qc.t  (qreg_q[0]).c_if(creg_c, 0b1000)
+qc.tdg(qreg_q[1]).c_if(creg_c, 0b0001)
+qc.tdg(qreg_q[1]).c_if(creg_c, 0b0010)
+qc.t  (qreg_q[1]).c_if(creg_c, 0b0100)
+qc.t  (qreg_q[1]).c_if(creg_c, 0b1000)
+
+qc.h(qreg_q[0])
 qc.h(qreg_q[1])
 
-qc.draw()
+print(qc.draw())
 
 def getIndices(inputs) -> List[int]:
 	indices = []
-
 	for index, i in enumerate(inputs):
 		# ! Wild expression to determine if bits are alternating: O(1) !
 		if (math.log2((i ^ ((i << 1) + (0 if i % 2 else 1))) + 1) % 1 == 0) and i > 3:
@@ -61,10 +69,13 @@ def getQB(inputs):
 	indices = getIndices(inputs)
 	if not len(indices):
 		return None
+	# TODO: Put indices into the cbit
+	# Get bloch
 	backend_sim = Aer.get_backend('statevector_simulator')
 	result = execute(qc, backend_sim).result()
 	state = result.get_statevector(qc)
 	plot_bloch_multivector(state)
+	plt.show()
 
 if __name__ == '__main__':
 	vec = [1, 5, 7, 10]
